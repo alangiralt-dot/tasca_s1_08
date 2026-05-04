3583e9500c2fdbe0be7e0a8fd74eb83e052573c4
Alan GIRALT <alangiralt@telefonica.net>
2026-05-04 13:47:22 +0200

    refactor: Write a method for each case by adding key information in the method identifier.

--- tests/Nivell1/Exercici1/NumberCheckerTest.php
+++ tests/Nivell1/Exercici1/NumberCheckerTest.php
@@ -4,23 +4,12 @@ namespace Alan\TascaS108\Tests\Nivell1\Exercici1;
 use PHPUnit\Framework\TestCase;
 use Alan\TascaS108\Nivell1\Exercici1\NumberChecker;
 class NumberCheckerTest extends TestCase {
-  public function testNumberChecker() {
-    $obj = new NumberChecker(-4);
-    $this->assertTrue($obj->isEven());
-    $this->assertFalse($obj->isPositive());    
-    
-    $obj = new NumberChecker(-3);
-    $this->assertFalse($obj->isEven());
-    
-    $obj = new NumberChecker(0);
-    $this->assertTrue($obj->isEven());
-    $this->assertFalse($obj->isPositive());    
-    
-    $obj = new NumberChecker(3);
-    $this->assertFalse($obj->isEven());
-    
-    $obj = new NumberChecker(4);
-    $this->assertTrue($obj->isEven());
-    $this->assertTrue($obj->isPositive());    
-  }
+  public function testIsEvenMinus4() {$this->assertTrue((new NumberChecker(-4))->isEven());}
+  public function testIsPositiveMinus4() {$this->assertFalse((new NumberChecker(-4))->isPositive());}
+  public function testIsEvenMinus3() {$this->assertFalse((new NumberChecker(-3))->isEven());}
+  public function testIsEvenZero() {$this->assertTrue((new NumberChecker(0))->isEven());}
+  public function testIsPositiveZero() {$this->assertFalse((new NumberChecker(0))->isPositive());}
+  public function testIsEven3() {$this->assertFalse((new NumberChecker(3))->isEven());}
+  public function testIsEven4() {$this->assertTrue((new NumberChecker(4))->isEven());}
+  public function testIsPositive4() {$this->assertTrue((new NumberChecker(4))->isPositive());}
 }
--- tests/Nivell1/Exercici2/SpeedSensorTest.php
+++ tests/Nivell1/Exercici2/SpeedSensorTest.php
@@ -4,15 +4,13 @@ namespace Alan\TascaS108\Tests\Nivell1\Exercici2;
 use PHPUnit\Framework\TestCase;
 use Alan\TascaS108\Nivell1\Exercici2\SpeedSensor;
 class SpeedSensorTest extends TestCase{
-  public function testSpeedSensor(){
-    $this->assertEquals("molt lent", (new SpeedSensor(5))->getEvaluation());
-    $this->assertEquals("velocitat adequada", (new SpeedSensor(30))->getEvaluation());
-    $this->assertEquals("velocitat adequada", (new SpeedSensor(42))->getEvaluation());
-    $this->assertEquals("velocitat adequada", (new SpeedSensor(60))->getEvaluation());
-    $this->assertEquals("excés lleu", (new SpeedSensor(74))->getEvaluation());
-    $this->assertEquals("excés lleu", (new SpeedSensor(80))->getEvaluation());
-    $this->assertEquals("excés moderat", (new SpeedSensor(92))->getEvaluation());
-    $this->assertEquals("excés moderat", (new SpeedSensor(100))->getEvaluation());
-    $this->assertEquals("excés greu", (new SpeedSensor(160))->getEvaluation());
-  }
+  public function testGetEvaluation5() {$this->assertEquals("molt lent", (new SpeedSensor(5))->getEvaluation());}
+  public function testGetEvaluation30() {$this->assertEquals("velocitat adequada", (new SpeedSensor(30))->getEvaluation());}
+  public function testGetEvaluation42() {$this->assertEquals("velocitat adequada", (new SpeedSensor(42))->getEvaluation());}
+  public function testGetEvaluation60() {$this->assertEquals("velocitat adequada", (new SpeedSensor(60))->getEvaluation());}
+  public function testGetEvaluation74() {$this->assertEquals("excés lleu", (new SpeedSensor(74))->getEvaluation());}
+  public function testGetEvaluation80() {$this->assertEquals("excés lleu", (new SpeedSensor(80))->getEvaluation());}
+  public function testGetEvaluation92() {$this->assertEquals("excés moderat", (new SpeedSensor(92))->getEvaluation());}
+  public function testGetEvaluation100() {$this->assertEquals("excés moderat", (new SpeedSensor(100))->getEvaluation());}
+  public function testGetEvaluation160() {$this->assertEquals("excés greu", (new SpeedSensor(160))->getEvaluation());}
 }
